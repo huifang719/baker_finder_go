69eb9021a6f6308b77012598cf7443a9373d877d
livia <livia.gu@notitia.consulting>
2024-03-19 21:13:14 +1030

    set up db schema

---
+++ db/schema/001_schema.sql
@@ -0,0 +1,31 @@
+-- +goose Up
+CREATE TABLE users (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL,
+  user_name TEXT NOT NULL,
+  user_type TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password_digest TEXT NOT NULL
+);
+
+CREATE TABLE bakers(
+    id SERIAL PRIMARY KEY,
+    img TEXT,
+    name TEXT,
+    address TEXT,
+    suburb TEXT,
+    postcode TEXT,
+    contact TEXT,
+    specialty TEXT,
+    creator TEXT
+);
+
+CREATE TABLE reviews(
+  id SERIAL PRIMARY KEY,
+  baker_id NUMERIC,
+  review TEXT,
+  rating NUMERIC,
+  user_name TEXT
+);
+-- +goose Down
